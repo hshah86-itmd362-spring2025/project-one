d978e30555b39e5f91b2279ead5e7fbd2a665c06
Hashir <70011762+HashMaster02@users.noreply.github.com>
2025-02-06 22:51:37 -0600

    added functionality to display a confirmation message after signing up

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const formElem = document.querySelector("form");
+const rightSection = document.querySelector(".right-section");
 
 formElem.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(e.target);
   const formObject = Object.fromEntries(formData.entries());
   console.log(formObject);
+
+  rightSection.innerHTML = `
+
+  
+    <h1 class="completed-header">Thank you for Signing Up ${formObject.first_name}!</h1>
+    <p class="email-confirmation">
+        We've sent you an email to ${formData.email}. Click the link in there to
+        verify your email.
+    </p>
+  
+  `;
 });
